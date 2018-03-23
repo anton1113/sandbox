6355077d5dffa392ec224f79c7595adf5466389e
anton1113 <anton11131113@gmail.com>
2018-03-23 10:35:51 +0200

    initial commit

---
+++ src/main/java/com/arash/Sandbox.java
@@ -0,0 +1,13 @@
+package com.arash;
+
+/**
+ *
+ * @author anton1113
+ */
+public class Sandbox {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
